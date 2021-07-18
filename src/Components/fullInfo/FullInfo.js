5298c15bea52f51074aee043630c2887cbd94dfe
Mukola Dera <dns3211@gmail.com>
2021-07-18 11:07:21 +0300

    add contact

--- src/Components/fullInfo/FullInfo.js
+++ src/Components/fullInfo/FullInfo.js
@@ -9,13 +9,19 @@ const FullInfo = () => {
         <h2 className="full-info-title">Полезная литература</h2>
         <ul className="full-info-list">
           <li className="full-info-item">
-            <a href="/"> 1. Тестирование dot.com Савин.</a>
+            <a href="https://codernet.ru/books/qa/testirovanie_dot_com_roman_savin_2007/">
+              1. Тестирование dot.com Савин.
+            </a>
           </li>
           <li className="full-info-item">
-            <a href="/"> 2. Психбольница в руках пациентов.</a>
+            <a href="https://habr.com/ru/post/248063/">
+              2. Психбольница в руках пациентов.
+            </a>
           </li>
           <li className="full-info-item">
-            <a href="/"> 3. Scrum. Дж. Сазерленд.</a>
+            <a href="https://www.mann-ivanov-ferber.ru/assets/files/bookparts/scrum/scrum_25-5-17_read_stamped.pdf">
+              3. Scrum. Дж. Сазерленд.
+            </a>
           </li>
         </ul>
         <h2 className="full-info-title">Полезные ресурсы</h2>
@@ -27,7 +33,9 @@ const FullInfo = () => {
             <a href="https://habr.com/ru">2. Habr</a>
           </li>
           <li className="full-info-item">
-            <a href="/">3. facebook.com/QA</a>
+            <a href="https://www.facebook.com/groups/370496047446051/">
+              3. facebook.com/QA
+            </a>
           </li>
           <li className="full-info-item">
             <a href="https://goit.ua/">4. goit.ua</a>
